9089a52223a091428ac0f4a0ee5a9be584e8436d
andrew21-mch <nfonandrew73@gmail.com>
2022-05-26 00:03:53 +0100

    tried the post api in flask

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,12 +3,23 @@ from flask import Flask
 import flask
 import requests
 app = Flask(__name__)
+from sqlalchemy import create_engine
+
+
+from auth import Auth
+
+
+AUTH = Auth()
 
 @app.route("/")
 def home():
     data = {"message": "Hello World!"}
     return flask.jsonify(data)
 
+@app.route('/user')
+def user():
+    r = requests.post(AUTH.register_user("drew12@gmail.com", "12342"))
+    return flask.jsonify(r)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
