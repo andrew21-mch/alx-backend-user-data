3f14e927cd6a948ee399aa0e8483756536703b6f
andrew21-mch <nfonandrew73@gmail.com>
2022-05-25 22:47:27 +0100

    simple flask app to return json payload

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import flask
+import requests
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    data = {"message": "Hello World!"}
+    return flask.jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
