0e909b47d9fde478ffd862e96bd165328c457cff
andrew21-mch <nfonandrew73@gmail.com>
2022-05-25 21:52:07 +0100

    add complete add user function

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,7 +32,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ saves a user to the database and returns a user object """
+        """method saves a user to the database and returns a user object """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
